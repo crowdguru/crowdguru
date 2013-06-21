3bbfbf562759cd61340a49911863ec829b681e6b
Koray Gulcu <kabexxnuf@gmail.com>
2013-06-21 01:07:17 +0100

    Reverting the last commit.

--- webapp/src/test/java/org/crowdguru/webapp/controllers/SkeletalControllerTest.java
+++ webapp/src/test/java/org/crowdguru/webapp/controllers/SkeletalControllerTest.java
@@ -1,14 +1,12 @@
 package org.crowdguru.webapp.controllers;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 
 public class SkeletalControllerTest {
 
 	@Test
 	public void testAllGurus() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 }
