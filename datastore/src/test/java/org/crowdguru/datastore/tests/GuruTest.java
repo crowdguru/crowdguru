27b6d94dd19084757ebd9a170539913e93c373c8
Koray Gulcu <kabexxnuf@gmail.com>
2013-06-30 03:49:53 +0100

    Enabled unit tests in datastore module
    Details
    -------
    This makes maven run unit tests automatically on builds.
    Code instrumentation is also enabled.
    
    Ticket
    ------
    closed #12

--- datastore/src/test/java/org/crowdguru/datastore/tests/GuruTest.java
+++ datastore/src/test/java/org/crowdguru/datastore/tests/GuruTest.java
@@ -2,6 +2,7 @@ package org.crowdguru.datastore.tests;
 
 import org.crowdguru.datastore.domain.User;
 import org.crowdguru.datastore.helpers.GuruHelper;
+import org.crowdguru.datastore.helpers.impl.GuruHelperImpl;
 import org.crowdguru.datastore.validators.GuruValidator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,29 +11,28 @@ import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@Component
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({ "/datastoreContext.xml" })
-public class GuruTests extends BaseTest {
-	
+@ContextConfiguration({ "/datastoreContext.xml", "/datastoreTestContext.xml" })
+public class GuruTest extends BaseTest {
+
 	@Test
 	public void testGuru1() {
 		User guru = guruHelper.guru1();
 		guruValidator.validateGuru1(guru);
 		log().info("Got unattached guru '" + guru + "'");
-		
-//		Guru merged = getGuruRepository().save(guru);
-//		guruValidator.validateGuru1(merged);
-//		log().info("Got merged guru '" + merged + "'");
-		
-//		Guru retrieved = getGuruRepository().findOne(merged.getNodeId());
-//		guruValidator.validateGuru1(retrieved);
-//		log().info("Got retrieved guru '" + retrieved + "'");
+
+		// Guru merged = getGuruRepository().save(guru);
+		// guruValidator.validateGuru1(merged);
+		// log().info("Got merged guru '" + merged + "'");
+
+		// Guru retrieved = getGuruRepository().findOne(merged.getNodeId());
+		// guruValidator.validateGuru1(retrieved);
+		// log().info("Got retrieved guru '" + retrieved + "'");
 	}
-	
+
 	@Autowired
 	private GuruHelper guruHelper;
-	
+
 	@Autowired
 	private GuruValidator guruValidator;
 }
